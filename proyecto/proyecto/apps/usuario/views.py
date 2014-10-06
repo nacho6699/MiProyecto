049141ac42226deaa18878f0cf83672b018a40af
jose <cruzi3836@gmail.com>
2014-10-06 17:05:22 -0400

    creacion registro user y mejorando

--- proyecto/proyecto/apps/usuario/views.py
+++ proyecto/proyecto/apps/usuario/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
+from .forms import *
 # Create your views here.
 def view_principal(request):
 	return render_to_response("inicio.html", {},context_instance=RequestContext(request))
 
 def view_registrar(request):
-	return render_to_response("usuario/registrar.html", {},context_instance=RequestContext(request))
+	formularioRegistro=fUsuario()
+	return render_to_response("usuario/registrar.html", {"formulario":formularioRegistro},context_instance=RequestContext(request))
