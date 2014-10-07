21c226fe59bd45af1f96f3c26d6e863b6ca54d8e
jose <cruzi3836@gmail.com>
2014-10-07 15:12:53 -0400

    aumentando

--- proyecto/proyecto/apps/usuario/views.py
+++ proyecto/proyecto/apps/usuario/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from .forms import *
 from .models import *
+
 # Create your views here.
 def view_principal(request):
 	return render_to_response("inicio.html", {},context_instance=RequestContext(request))
@@ -16,8 +17,8 @@ def view_registrar(request):
 			nuevo_usuario=request.POST['username']
 			formularioRegistro.save()
 			usuario=User.objects.get(username=nuevo_usuario)
-			usuario.is_active=False
-			usuario.save()
+			
+			
 			perfil=Perfil.objects.create(user=usuario)
 			return HttpResponse("Registrado")
 	else:
